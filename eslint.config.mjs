f07cb1884f04ba407da2ee74496f27030863478e
Tsz Cheung Tsui <steric.tsui@mail.utoronto.ca>
2026-05-02 17:10:06 -0400

    Ignore generated worktree files in ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,20 @@
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 
-const eslintConfig = [...nextVitals, ...nextTs];
+const eslintConfig = [
+  {
+    ignores: [
+      ".next/**",
+      ".worktrees/**",
+      "coverage/**",
+      "dist/**",
+      "node_modules/**",
+      "playwright-report/**",
+      "test-results/**",
+    ],
+  },
+  ...nextVitals,
+  ...nextTs,
+];
 
 export default eslintConfig;
